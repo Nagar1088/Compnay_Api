39dde41137174e86b3f0836a8836c1cbebe55d5f
nagar1088 <sachinnagar094@gmail.com>
2024-03-13 10:59:10 +0530

    django project

---
+++ companyapi/views.py
@@ -0,0 +1,11 @@
+# from django.http import JsonResponse
+
+
+# def home_page(request):
+#     print("home page requested")
+#     friends=[
+#         'ankit',
+#         'soni',
+#         'Piller'
+#     ]
+#     return JsonResponse(friends,safe=False)
